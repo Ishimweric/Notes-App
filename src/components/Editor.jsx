0a630f63f0085c493fc3c88344a61eef34c9065c
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-07 10:39:57 +0200

    Fixed the buggy Markdown Editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactMde from "react-mde"
 import Showdown from "showdown"
 import '../index.css'
+import "react-mde/lib/styles/css/react-mde-all.css";
 
 export default function Editor({ currentNote, updateNote }) {
     const [selectedTab, setSelectedTab] = React.useState("write")
